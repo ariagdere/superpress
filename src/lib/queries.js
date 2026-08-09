a8b88b349720b7d77035d75c98f7d3f1b7dca86e
ariagdere <ariagdere@gmail.com>
2026-08-09 21:31:49 +0300

    Update queries.js

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -273,7 +273,7 @@ export async function getCategoriesWithCounts(db, lang) {
     )
     .bind(BRAND)
     .all();
-  return results.filter((c) => c.product_count > 0);
+  return results;
 }
 
 function formatRange(set, isNumeric) {
